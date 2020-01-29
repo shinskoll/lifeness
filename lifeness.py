aa854702dc5b5bcc738fbc7f3c3823d64da035f4
shinskoll <shinskoll@gmail.com>
2020-01-30 00:24:15 +0100

    temp python

--- lifeness.py
+++ lifeness.py
@@ -1,11 +1,19 @@
 from guizero import App, Box, PushButton, ListBox, TextBox
+import io
 
 app = App(title="Hellow lord")
 
 filename = ""
 
-def get_file():
-    filename = app.select_file()
+def getfile():
+    filename = app.select_file(filetypes=[["All files", "*.*"], ["Text documents", "*.txt"]])
+    f = open(filename, "r", encoding="utf-8")
+    entry.value = f.read()
+
+def savefile():
+    filename = app.select_file(save=True)
+    with open(filename, 'w') as file:
+        file.write(entry.value)
 
 def checklist():
     listbox.clear()
@@ -15,8 +23,8 @@ def checklist():
             listbox.append(i)
 
 buttonsBox = Box(app, width="fill", align="top", height=25)
-load = PushButton(buttonsBox, text="Load", align="left")
-save = PushButton(buttonsBox, text="Save", align="left")
+load = PushButton(buttonsBox, text="Load", command=getfile, align="left")
+save = PushButton(buttonsBox, text="Save", command=savefile, align="left")
 listbox = ListBox(app, items=[], height="fill", align="left", width=100)
 entry = TextBox(app, width="fill", height="fill", multiline=True, scrollbar=True)
 
